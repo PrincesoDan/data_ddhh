e21a64810eb458afb5e6227ab35d2f7b2480e238
Felipe Salas <felipe.salas.b@gmail.com>
2024-08-10 17:59:47 -0400

    title

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+
+def main():
+    _, bigcol, _ = st.columns([1, 10, 1])
+    with bigcol:
+        st.title("Detenidos desaparecidos en Chile durante la dictadura de Pinochet")
